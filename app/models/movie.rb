1107bba0ce3acc9030f7be94aa6f4f60d6d018b8
MoneyJohne <change@me.com>
2015-06-25 17:22:15 -0700

    Added PostgreSQL

---
+++ app/models/movie.rb
@@ -0,0 +1,8 @@
+class Movie < ActiveRecord::Base
+	searchkick
+	belongs_to :user
+	has_many :reviews
+
+	has_attached_file :image, styles: { medium: "180x267#" }
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+end
